c41bece57a00970db133bf1d53d175ddb9237ec3
joaocamargodev <joao.felipe.camargo.jfc@gmail.com>
2025-07-03 23:50:42 -0300

    incomplete, just autosave

--- javascript.js
+++ javascript.js
@@ -1,43 +1,48 @@
+
+
+
+
+
+
+
 const btnmenu = document.getElementById('btn-menu');
 const backmenu = document.getElementById('back-menu');
 const menu = document.querySelector('nav ul');
 
 btnmenu.addEventListener('click', () => {
-    menu.style.display = 'flex';
+    menu.style = 'opacity: 1; transition: opacity 0.2s ease-in-out;';
     btnmenu.style.display = 'none';
     backmenu.style.display = 'block';
 });
 
 backmenu.addEventListener('click', () => {
-    menu.style.display = 'none';
+    menu.style = 'opacity: 0; transition: opacity 0.2s ease-in-out;';
     btnmenu.style.display = 'block';
     backmenu.style.display = 'none';
 });
 
 // Solução: limpa os estilos inline ao redimensionar a tela
 window.addEventListener('resize', () => {
-    menu.style.display = '';
+    menu.style.opacity = '';
     btnmenu.style.display = '';
     backmenu.style.display = '';
 });
 
 const seta = document.getElementById('notion');
 const setaImg = document.getElementById('seta-1');
-seta.addEventListener('click', () => {
-    if (setaImg.style.transform === 'rotate(180deg)') {
-        setaImg.style.transform = 'rotate(0deg)';
-    } else {
-        setaImg.style.transform = 'rotate(180deg)';
-    }
+seta.addEventListener('mouseover',() => { 
+    setaImg.style.transform = 'rotate(180deg)';
+});
+seta.addEventListener('mouseout',() => {
+    setaImg.style.transform = 'rotate(0deg)';
 });
 
 const seta2 = document.getElementById('explorar');
 const setaImg2 = document.getElementById('seta-2');
 
-seta2.addEventListener('click', () => {
-    if (setaImg2.style.transform === 'rotate(180deg)') {
-        setaImg2.style.transform = 'rotate(0deg)';
-    } else {
-        setaImg2.style.transform = 'rotate(180deg)';
-    }
+seta2.addEventListener('mouseover',() => { 
+    setaImg2.style.transform = 'rotate(180deg)';
+});
+seta2.addEventListener('mouseout',() => {
+    setaImg2.style.transform = 'rotate(0deg)';
 });
